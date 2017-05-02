b62ebf81d6f1b4165dfa2027b4996ae97cf07631
VictoriaVasys <vvasys@gmail.com>
2017-05-02 15:15:11 -0600

    Add Find controller namespaced under Invoices

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      get 'invoices/random', to: 'invoices#show'
-      get 'invoices/find', to: 'invoices#show'
-      get 'invoices/find_all', to: 'invoices#index'
+      namespace :invoices do
+        get 'random', to: 'find#show'
+        get 'find', to: 'find#show'
+        get 'find_all', to: 'find#index'
+      end
       resources :invoices, only: [:index, :show]
     end
   end
