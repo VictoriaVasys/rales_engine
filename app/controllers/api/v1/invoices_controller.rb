a4efbb97e3d6242ca2e0ff9a8c71fe6315b5217b
VictoriaVasys <vvasys@gmail.com>
2017-05-02 15:07:55 -0600

    Create invoices controller and tests for all record endpoints

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,9 +1,26 @@
 class Api::V1::InvoicesController < ApplicationController
   def index
-    render json: Invoice.all
+    find_all_params(invoice_params.keys.first)
   end
 
   def show
-    render json: Invoice.find(params[:id])
+    if !invoice_params.empty?
+      find_params(invoice_params.keys.first)
+    else
+      render json: Invoice.order("RANDOM()").first
+    end
+  end
+  
+  private
+  def find_params(attribute)
+    render json: Invoice.find_by("#{attribute}": params[:"#{attribute}"])
+  end
+  
+  def find_all_params(attribute)
+    render json: Invoice.where("#{attribute}": params[:"#{attribute}"])
+  end
+  
+  def invoice_params
+    params.permit(:id, :status, :created_at, :updated_at, :merchant_id, :customer_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
+      get 'invoices/random', to: 'invoices#show'
+      get 'invoices/find', to: 'invoices#show'
+      get 'invoices/find_all', to: 'invoices#index'
       resources :invoices, only: [:index, :show]
     end
   end
