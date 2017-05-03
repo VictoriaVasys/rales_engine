3e31d45be7f6da968c41956e1bb5db8ebcdb9ecd
Victoria Vasys <victoria.vasys@gmail.com>
2017-05-03 11:01:32 -0600

    remove comments

--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -1,29 +1,3 @@
 class InvoiceSerializer < ActiveModel::Serializer
   attributes :id, :status, :customer_id, :merchant_id
-
-  # def customer
-  #   {id: object.customer.id}
-  # end
-  #
-  # def merchant
-  #   {id: object.merchant.id}
-  # end
-  #
-  # def invoice_items
-  #   object.invoice_items.map do |invoice_item|
-  #     {id: invoice_item.id}
-  #   end
-  # end
-  # def transactions
-  #   object.transactions.map do |transaction|
-  #     {id: transaction.id}
-  #   end
-  # end
-  # def items
-  #   object.items.map do |item|
-  #     {id: item.id}
-  #   end
-  # end
-
-
 end
