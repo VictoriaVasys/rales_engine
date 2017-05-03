c0a8b25185910fa97400f9b63b172cbca83dfa54
Jack Pincus <jwpincus@gmail.com>
2017-05-03 10:59:29 -0600

    add record enpoint for customer

--- spec/requests/api/v1/customers/customer_request_spec.rb
+++ spec/requests/api/v1/customers/customer_request_spec.rb
@@ -20,48 +20,31 @@ describe "Customer API" do
     expect(response).to be_success
     expect(customer["id"]).to eq(id)
   end
-  # it "can find one invoice by its customer_id" do
-  #   customer_id = Fabricate(:invoice).customer_id
-  #
-  #   get "/api/v1/invoices/find?customer_id=#{customer_id}"
-  #   invoice = JSON.parse(response.body)
-  #   expect(response).to be_success
-  #   expect(invoice["customer"]["id"]).to eq(customer_id)
-  # end
-  # it "can find one invoice by its merchant_id" do
-  #   merchant_id = Fabricate(:invoice).merchant_id
-  #
-  #   get "/api/v1/invoices/find?merchant_id=#{merchant_id}"
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoice['merchant']['id']).to eq(merchant_id)
-  # end
-  # it "can find one invoice by its status" do
-  #   status = Fabricate(:invoice).status
-  #
-  #   get "/api/v1/invoices/find?status=#{status}"
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoice["status"]).to eq(status)
-  # end
-  # it "can find one invoice by its id" do
-  #   id = Fabricate(:invoice).id
-  #
-  #   get "/api/v1/invoices/find?id=#{id}"
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoice["id"]).to eq(id)
-  # end
-  # it "can find all invoices by customer_id" do
-  #   id = Fabricate(:customer).id
-  #   get "/api/v1/invoices/find_all?customer_id=#{id}"
-  #   invoices = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoices.count).to eq(3)
-  # end
+  it "can find one customer by its customer_id" do
+    customer_id = Fabricate(:customer).id
+
+    get "/api/v1/customers/find?id=#{customer_id}"
+    customer = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(customer["id"]).to eq(customer_id)
+  end
+  it "can find one customer by its first_name" do
+    first_name = Fabricate(:customer).first_name
+
+    get "/api/v1/customers/find?first_name=#{first_name}"
+    customer = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(customer["first_name"]).to eq(first_name)
+  end
+  it "can find one customer by its last_name" do
+    last_name = Fabricate(:customer).last_name
+
+    get "/api/v1/customers/find?last_name=#{last_name}"
+    customer = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(customer["last_name"]).to eq(last_name)
+  end
+  
+
 
 end
