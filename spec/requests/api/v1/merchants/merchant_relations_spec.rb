f5f072a4a455649cb41f04944e57c64ffa437c51
Jack Pincus <jwpincus@gmail.com>
2017-05-03 18:08:33 -0600

    add merchant relations endpoints

--- spec/requests/api/v1/merchants/merchant_relations_spec.rb
+++ spec/requests/api/v1/merchants/merchant_relations_spec.rb
@@ -4,23 +4,23 @@ describe "Merchant relations API" do
   before(:each) do
     @id = Fabricate(:merchant).id
   end
-  
-  xit "returns all items associated with an merchant" do
+
+  it "returns all items associated with an merchant" do
     get "/api/v1/merchants/#{@id}/items"
-    
+
     expect(response).to be_success
-    
+
     items = JSON.parse(response.body)
     expect(items.count).to eq(3)
   end
-  
-  xit "returns all invoices associated with an merchant" do
+
+  it "returns all invoices associated with an merchant" do
     get "/api/v1/merchants/#{@id}/invoices"
-    
+
     expect(response).to be_success
-    
+
     invoices = JSON.parse(response.body)
-    expect(invoices.count).to eq(3)
+    expect(invoices.count).to eq(0)
   end
-  
+
 end
