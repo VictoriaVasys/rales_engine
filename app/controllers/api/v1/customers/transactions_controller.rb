c19ccb6898503ea8968961cf08d2044396701139
Victoria Vasys <victoria.vasys@gmail.com>
2017-05-04 10:59:54 -0600

    Break up the sql query syntactically

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::Customers::TransactionsController < ApplicationController
 
   def index
-    render json: Transaction.find_by_sql("select transactions.* from transactions, customers, invoices where transactions.invoice_id = invoices.id and invoices.customer_id = customers.id and customers.id = #{params[:customer_id]}")
+    render json: Transaction.find_by_sql("
+      select transactions.* from transactions, customers, invoices 
+      where transactions.invoice_id = invoices.id 
+      and invoices.customer_id = customers.id 
+      and customers.id = #{params[:customer_id]}
+      ")
   end
 
 end
